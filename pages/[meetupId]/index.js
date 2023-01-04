6bf8b4983ca485e034cd027e6e64b411cc4c2e5b
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-04 18:04:41 +0100

    add detail content with module css styling

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail 
+            image='https://cdn.pixabay.com/photo/2016/11/06/23/51/buildings-1804481_960_720.jpg' 
+            title='First Meetup' 
+            address='Some address' 
+            description='description'
+        />
+    );
+}
+
+export default MeetupDetails;
