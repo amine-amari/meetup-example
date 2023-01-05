c7e4acf3b519695a2fca781d1c36f8b0f4fcd15a
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-05 15:17:58 +0100

    add api route for new meetups

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,13 @@
+// /api/new-mmetup
+// POST /api/new-mmetup
+
+function handler(req, res) {
+    if (req.method === 'POST') {
+        const data = req.body;
+
+        const {title, image, address, description} = data;
+
+    } 
+}
+
+export default handler;
