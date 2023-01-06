c68d7ca4a44d71324379fc2b72ebdbf20f3e32cb
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-06 14:21:47 +0100

    add meta data

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,22 @@
 import MeetupList from '../components/meetups/MeetupList';
 import { MongoClient } from 'mongodb';
-
-const dummy_meetups = [
-    {
-        id: 'm1',
-        title: 'First Meetup',
-        image: 'https://cdn.pixabay.com/photo/2016/11/06/23/51/buildings-1804481_960_720.jpg',
-        address: 'Some address 7, 7777 City',
-        description: 'This is a first meetup'
-    },
-    {
-        id: 'm2',
-        title: 'Second Meetup',
-        image: 'https://cdn.pixabay.com/photo/2016/11/06/23/51/buildings-1804481_960_720.jpg',
-        address: 'Some address 2, 7777 City',
-        description: 'This is a second meetup'
-    }
-];
+import Head from 'next/head';
+import { Fragment } from 'react';
 
 function HomePage(props) {
 
-    return <MeetupList meetups={props.meetups} /> 
+    return (
+        <Fragment>
+            <Head>
+                <title>Meetup Example</title>
+                <meta 
+                    name='descirption' 
+                    content='highly active Meetups'
+                />
+            </Head>
+            <MeetupList meetups={props.meetups} /> 
+        </Fragment>
+    );  
 }
 
 // Server Site rendering
